c81f9e75387b0d32b21b42e4230696c2909ab2d5
Hamid Mahmoodabadi <mahmoodabadihamid@gmail.com>
2023-05-08 09:10:24 +0330

    python module added
    python module for converting excel to json added

---
+++ demo/public/excel_to_json.py
@@ -0,0 +1,38 @@
+import random
+import pandas as pd
+import hazm
+norm = hazm.Normalizer()
+excel_file = 'data.xlsx'
+
+df_edges = pd.read_excel(excel_file, sheet_name=0, header=0)
+edges = df_edges.values.tolist()
+
+df_nodes = pd.read_excel(excel_file, sheet_name=1, header=0)
+df_nodes['cluster'] = df_nodes['cluster'].apply(str)
+nodes = df_nodes.to_dict('records')
+
+clusters_key=[]
+clusters = ""
+random.seed(2)
+r = lambda: random.randint(0,255)
+for i in zip(df_nodes['cluster'].values, df_nodes['tag'].values):
+    if str(i[0]) not in clusters_key:
+        random_color = '#%02X%02X%02X' % (r(),r(),r())
+        clusters+= ", "+ str({"key": str(i[0]), "color": random_color, "clusterLabel": i[1]})
+        clusters_key.append(str(i[0]))
+clusters = clusters[1:]
+
+
+
+json_str = """{
+  "nodes": 
+  """ + str(nodes) + """ ,
+  "edges": """ + str(edges) + """  ,
+  "clusters": [ """+clusters+""" ],
+  "tags": [
+    {"key": "مالی", "image": "charttype.svg" }
+    
+  ]
+}"""
+
+_ = open('dataset.json', 'w', encoding='utf-8').write(str(json_str).replace("'", '"'))
